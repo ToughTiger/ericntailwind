564368f06b94c351ce49117319b647f0c231604c
toughtiger <santosh.cerdak@gmail.com>
2025-10-24 19:18:31 +0530

    Updated Publish action

--- app/Filament/Resources/LinkedInPostResource.php
+++ app/Filament/Resources/LinkedInPostResource.php
@@ -14,6 +14,7 @@ use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Database\Eloquent\SoftDeletingScope;
 use Filament\Notifications\Notification;
 use Illuminate\Support\Facades\Log;
+use Illuminate\Support\Str;
 class LinkedInPostResource extends Resource
 {
     protected static ?string $model = LinkedInPost::class;
